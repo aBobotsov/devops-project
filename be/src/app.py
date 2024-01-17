2ad45b1f2f166d09be2aa0f120739107762a4c78
aBobotsov <a.bobotsov@gmail.com>
2024-01-17 23:22:36 +0200

    add reminder

--- be/src/app.py
+++ be/src/app.py
@@ -4,6 +4,7 @@ from flask_cors import cross_origin
 
 app = Flask(__name__)
 
+
 @app.route('/validate', methods=['POST'])
 @cross_origin()
 def validate_input():
@@ -11,13 +12,13 @@ def validate_input():
         data = request.get_json()
         input_value = data.get('input')
 
-        # Perform your validation logic here
-        # For example, you can check if the input is an even number
+        # TODO: implement real logic
         is_valid = int(input_value) % 2 == 0
 
         return jsonify({'isValid': is_valid})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(port=os.environ.get("PORT", 5000), host="0.0.0.0")

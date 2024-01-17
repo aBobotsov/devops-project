9b341e3addd560b284c482c903a1f14f6d3519c8
aBobotsov <a.bobotsov@gmail.com>
2024-01-17 09:46:27 +0200

    be unit-tests

---
+++ be/test/README.md
@@ -0,0 +1,11 @@
+## Pre-requisites:
+From the `be` root folder install requirements via
+```sh
+pip install -r test/requirements.txt
+```
+ 
+## Running all tests:
+From the `be` root folder run
+```sh
+pytest test/test_app.py
+```
---
+++ be/test/test_app.py
@@ -0,0 +1,45 @@
+import json
+from flask.testing import FlaskClient
+import pytest
+
+from be.src.app import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_valid_input(client: FlaskClient):
+    response = client.post(
+        '/validate',
+        data=json.dumps({'input': '2'}),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    assert response.status_code == 200
+    assert data['isValid']
+
+
+def test_invalid_input(client: FlaskClient):
+    response = client.post(
+        '/validate',
+        data=json.dumps({'input': '3'}),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    assert response.status_code == 200
+    assert not data['isValid']
+
+
+def test_missing_input(client: FlaskClient):
+    response = client.post(
+        '/validate',
+        data=json.dumps({}),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    assert response.status_code == 500
+    assert 'error' in data

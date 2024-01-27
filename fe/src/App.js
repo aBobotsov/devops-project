b1677533fc37556fcbfb14c0dcffe6e60664a4a3
aBobotsov <a.bobotsov@gmail.com>
2024-01-27 14:08:21 +0200

    trigger fe

--- fe/src/App.js
+++ fe/src/App.js
@@ -25,6 +25,7 @@ function App() {
       if (!response.ok) {
         throw new Error(`Validation failed: ${response.statusText}`);
       }
+
       const data = await response.json();
 
       // Update validation result based on backend response
